01d289955a34f71fe07c6ec4a7b0e3433c5f200b
Jaime Gilligan <gilligan.jme@gmail.com>
2017-02-09 20:01:59 -0600

    fixed issues

--- main.py
+++ main.py
@@ -15,26 +15,28 @@
 # limitations under the License.
 #
 import webapp2
+import re
+import cgi
 
-def build_page(user_input):
+def build_page(user_input = {"user" : "", "password":"", "verify": "", "email":"", "user_error":"", "password_error":"", "verify_error":"", "email_error":""}):
     user_label = "<label>Username\t</label>"
-    user = "<input type='text' name='user'/>"
-    user_error = "<span></span>"
+    user = "<input type='text' name='user' value='%(user)s'/>"
+    user_error = "<span>%(user_error)s</span>"
     user_area = user_label + user + user_error
 
     password_label = "<label>Password\t</label>"
     password = "<input type='text' name='password'/>"
-    password_error = "<span></span>"
+    password_error = "<span>%(password_error)s</span>"
     password_area = password_label + password + password_error
 
     verify_label = "<label>Verify Password\t</label>"
     verify = "<input type='text' name='verify'/>"
-    verify_error = "<span></span>"
+    verify_error = "<span>%(verify_error)s</span>"
     verify_area = verify_label + verify + verify_error
 
     email_label = "<label>Email (optional)\t</label>"
-    email = "<input type='text' name='email'/>"
-    email_error = "<span></span>"
+    email = "<input type='text' name='email' value='%(email)s'/>"
+    email_error = "<span>%(email_error)s</span>"
     email_area = email_label + email + email_error
 
     submit = "<input type='submit'/>"
@@ -42,19 +44,69 @@ def build_page(user_input):
     form = "<form method='post'>" + user_area + "<br/>" + password_area + "<br/>" + verify_area + "<br/>" + email_area + "<br/>" + submit
     header = "<h1>Sign-Up</h1>"
 
-    return header + form
+    return header + form % user_input
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    if email == "":
+        return True
+    return  email and EMAIL_RE.match(email)
 
 
-class MainHandler(webapp2.RequestHandler):
+class Index(webapp2.RequestHandler):
     def get(self):
-        content = build_page("")
+        content = build_page()
         self.response.write(content)
 
     def post(self):
+        success = True
+        user = cgi.escape(self.request.get("user"))
+        password = cgi.escape(self.request.get("password"))
+        verify = cgi.escape(self.request.get("verify"))
+        email = cgi.escape(self.request.get("email"))
+
+        user_input = {"user" : user, "email": email, "user_error" : "", "password_error" : "","email_error" : "","verify_error" : ""}
+        if not valid_username(user):
+            #self.response.write("User Failed")
+            user_input["user_error"] = "Invalid Username"
+            success = False
+
+        if not valid_password(password):
+            #self.response.write("password failed")
+            user_input["password_error"] = "Invalid Password"
+            success = False
+
+        if not valid_email(email):
+            user_input["email_error"] = "Invalid Email"
+            success = False
+
+        if password != verify:
+            user_input["verify_error"] = "Passwords do not match"
+            success = False
+
+        if success==False:
+             content = build_page(user_input)
+             self.response.write(content)
+        else:
+            self.redirect("/welcome?user=" + user)
+
+
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
         user = self.request.get("user")
 
-        self.response.write("Welcome, " + user)
+        self.response.write("Welcome, {}!".format(user))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/welcome', Welcome)
 ], debug=True)
